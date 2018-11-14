210e8e7089d1b7b834f4663b4b678cb871277aed
user <user@user-no-MacBook-Air.local>
2018-11-14 18:23:23 +0900

    add

---
+++ yosoku.py
@@ -0,0 +1,42 @@
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#気温データ10年分の読み込み
+df = pd.read_csv('kion10y.csv', encoding="utf-8")
+
+#データを学習用とテスト用に分割する
+train_year = (df["年"] <= 2015)
+test_year = (df["年"] >= 2016)
+interval = 6
+
+#過去6日分を学習するデータを作成
+def make_data(data):
+    x = []
+    y = []
+    temps = list(data["気温"])
+    for i in range(len(temps)):
+        if i <= interval: continue
+        y.append(temps[i])
+        xa = []
+        for p in range(interval):
+            d = i + p - interval
+            xa.append(temps[d])
+        x.append(xa)
+    return (x,y)
+
+train_x,train_y = make_data(df[train_year])
+test_x,test_y = make_data(df[test_year])
+
+#直線回帰分析を行う
+lr = LinearRegression(normalize=True)
+lr.fit(train_x,train_y) #学習
+pre_y = lr.predict(test_x) #予測
+
+#結果を図にプロット
+plt.figure(figsize=(10,6), dpi=100)
+plt.plot(test_y, c='r')
+plt.plot(pre_y, c='b')
+plt.savefig('tenki-heikin-tuki.png')
+plt.show()
